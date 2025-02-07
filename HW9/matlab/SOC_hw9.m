1725e8b867cdb1a8f3f878401e9cd85ec1db4ef9
AlmgoDob <dobalmog123@gmail.com>
2025-02-07 19:39:24 +0200

    done

--- HW9/matlab/SOC_hw9.m
+++ HW9/matlab/SOC_hw9.m
@@ -27,14 +27,23 @@ delta_a = 0.132*sind(2*(lambda_n_deg - lambda_s_deg))*tm
 %% Part C
 time_interval = 0:1:365*3;
 
-state = [lambda_n_deg; 0; a]; % [lambda, labmbda_dot, a]
+state = [lambda_n_deg; lambda_0_dot; a]; % [lambda, labmbda_dot, a]
+ % This is where we integrate the equations of motion.
+[t_out, state_out] = ode45(@ODE, time_interval, state, odeset('RelTol',5e-14,'AbsTol',5e-14));
+state_out_positive_lambda_0_dot = state_out;
 
+state = [lambda_n_deg; -lambda_0_dot; a]; % [lambda, labmbda_dot, a]
  % This is where we integrate the equations of motion.
 [t_out, state_out] = ode45(@ODE, time_interval, state, odeset('RelTol',5e-14,'AbsTol',5e-14));
 
-fig1 = figure ("Name","1",'Position',[100 300 900 500]);
+state_out_negative_lambda_0_dot = state_out;
 
-plot(t_out/365, state_out(:,1), "LineWidth",1.5)
+fig1 = figure ("Name","1",'Position',[100 300 900 500]);
+colors = cool(2);
+hold all
+
+plot(t_out/365, state_out_positive_lambda_0_dot(:,1), "LineWidth",1.5, "Color",colors(1,:))
+plot(t_out/365, state_out_negative_lambda_0_dot(:,1), "LineWidth",1.5, "Color",colors(2,:))
 
 xlabel('time [year]','FontSize',15,Interpreter='latex')
 ylabel('$\lambda$ [deg]','FontSize',15,Interpreter='latex')
@@ -42,7 +51,7 @@ grid on
 grid minor
 title("Drift due to The Tesseral Harmonics")
 subtitle("Almog Dobrescu 214254252")
-% legend({''},'FontSize',11 ,'Location','northwest')
+legend({'positive initial condition', 'negative initial condition'},'FontSize',15 ,'Location','northwest',Interpreter='latex')
 % exportgraphics(fig1, 'graph1.png','Resolution',300);
 
 
@@ -50,15 +59,25 @@ subtitle("Almog Dobrescu 214254252")
 %% Functions
 function d_state_dt = ODE(t, state)
     mu = 3.986e5; % [km^3/s^2]
-    
+
     lambda = state(1);
     lambda_dot = state(2);
     a = state(3);
     
     n = sqrt(mu/a^3);
 
-    a_dot = 6.06*sind(2*(lambda-27.8))-1.48*sind(4*(lambda-30.28))+0.19*sind(2*(lambda-31.92));
+    temp1 = abs(lambda-75);
+    temp2 = abs(lambda-255);
+    if (temp1<temp2)
+        lambda_s_deg = 75;
+    else
+        lambda_s_deg = 255;
+    end
 
-    d_state_dt = [lambda_dot; 3*n/4/a*a_dot; a_dot]; 
+    a_dot = 0.132*sind(2*(lambda-lambda_s_deg));
+
+    lambda_ddot = -3/2*n/a*a_dot;
+
+    d_state_dt = [lambda_dot; lambda_ddot; a_dot]; 
 end
 
